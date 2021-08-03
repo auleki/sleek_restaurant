aa3d8016f5f707d93edaebda3b5e4ff2151edb36
auleki <kodagiwa@gmail.com>
2021-08-03 23:01:58 +0100

    added scroll to reveal

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,7 +64,26 @@ function scrollTop () {
 window.addEventListener('scroll', scrollTop)
 /*   */
 
-/*   */
+/* SCROLL REVEAL  */
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 1000,
+  reset: true
+})
+
+sr.reveal(
+  `.home__data, .home__img,
+          .about__data, .about__img,
+          .services__content, .menu__content,
+          .app__data, .app__img,
+          .contact__data, .contact__button,
+          .footer__content`,
+  {
+    interval: 200
+  }
+)
+
 /*   */
 /*   */
 /*   */
