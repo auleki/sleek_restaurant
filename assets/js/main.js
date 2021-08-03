9b0f19cec57ea4664e88abfb71311d1e7efe9506
auleki <kodagiwa@gmail.com>
2021-08-03 01:26:28 +0100

    added shadow to navbar on scroll

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,42 @@ function removeMenu () {
   navMenu.classList.remove('show__menu')
 }
 navLink.forEach(n => n.addEventListener('click', removeMenu))
-/*   */
-/*   */
+
+/* SCROLL ACTIVE SECTIONS ACTIVE LINK  */
+const sections = document.querySelectorAll('section[id]')
+function scrollActive () {
+  const scrollY = window.pageYOffset
+  // console.log('OFFSET =>', scrollY)
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    let sectionId = current.getAttribute('id')
+    if (scrollY > sectionTop && scrollY < sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.add('active-link')
+    } else {
+      document
+        .querySelector('.nav__menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
+
+/* CHANGE BACKGROUND HEADER SIZE ON SCROLL  */
+function scrollHeader () {
+  const nav = document.getElementById('header')
+  if (this.scrollY >= 200) {
+    console.log(this.scrollY)
+    nav.classList.add('scroll-header')
+  } else {
+    nav.classList.remove('scroll-header')
+  }
+}
+
+window.addEventListener('scroll', scrollHeader)
+
 /*   */
 /*   */
