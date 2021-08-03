475cc4d9d7c641dcfe179e65f2fd66f5f37504ee
auleki <kodagiwa@gmail.com>
2021-08-03 01:03:29 +0100

    added remove menu on link click

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,16 @@ const showMenu = (toggleId, navId) => {
 }
 
 showMenu('nav-toggle', 'nav-menu')
+
+/* REMOVE MENU ON LINK CLICK  */
+
+const navLink = document.querySelectorAll('.nav__link')
+function removeMenu () {
+  const navMenu = document.getElementById('nav-menu')
+  navMenu.classList.remove('show__menu')
+}
+navLink.forEach(n => n.addEventListener('click', removeMenu))
+/*   */
+/*   */
+/*   */
+/*   */
